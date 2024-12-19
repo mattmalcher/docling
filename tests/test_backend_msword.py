13ee0db1bee3a4f54b8901d26e51b767635ebd60
Matthew Malcher <matmalcher@googlemail.com>
2024-12-19 19:01:51 +0000

    remove shortcut tests to allow merge of failing tests

--- tests/test_backend_msword.py
+++ tests/test_backend_msword.py
@@ -48,15 +48,6 @@ def get_docx_paths():
     pdf_files = sorted(directory.rglob("*.docx"))
     return pdf_files
 
-def get_docx_paths_numbered():
-
-    # Define the directory you want to search
-    directory = Path("./tests/data/docx/")
-
-    # List all PDF files in the directory and its subdirectories
-    pdf_files = sorted(directory.rglob("*numbered*.docx"))
-    return pdf_files
-
 
 def get_converter():
 
@@ -83,36 +74,6 @@ def verify_export(pred_text: str, gtfile: str):
 def test_e2e_docx_conversions():
 
     docx_paths = get_docx_paths()
-    converter = get_converter()
-
-    for docx_path in docx_paths:
-        # print(f"converting {docx_path}")
-
-        gt_path = (
-            docx_path.parent.parent / "groundtruth" / "docling_v2" / docx_path.name
-        )
-
-        conv_result: ConversionResult = converter.convert(docx_path)
-
-        doc: DoclingDocument = conv_result.document
-
-        pred_md: str = doc.export_to_markdown()
-        assert verify_export(pred_md, str(gt_path) + ".md"), "export to md"
-
-        pred_itxt: str = doc._export_to_indented_text(
-            max_text_len=70, explicit_tables=False
-        )
-        assert verify_export(
-            pred_itxt, str(gt_path) + ".itxt"
-        ), "export to indented-text"
-
-        pred_json: str = json.dumps(doc.export_to_dict(), indent=2)
-        assert verify_export(pred_json, str(gt_path) + ".json"), "export to json"
-
-
-def test_e2e_docx_conversions_numbered():
-
-    docx_paths = get_docx_paths_numbered()
     converter = get_converter()
 
     for docx_path in docx_paths:
